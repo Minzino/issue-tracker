0e7dd2ec04c56d3fb650efc2473f0662c5de7537
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-06-20 20:54:04 +0900

    깃헙액션 테스트

--- FE/issuetracker/.eslintrc.js
+++ FE/issuetracker/.eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'linebreak-style': 0,
     'import/prefer-default-export': 0,
-    'prettier/prettier': [1, { endOfLine: 'auto' }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'import/extensions': 0,
     'no-use-before-define': 0,
     'import/no-unresolved': 0,
